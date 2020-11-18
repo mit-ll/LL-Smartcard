ca1d56f89a4f8cbd5289d303a55c4fb0aabaa31a
Lou Bellaire <r.louis.bellaire@ll.mit.edu>
2020-11-18 15:13:13 -0500

    Update parser.py
    Adding MIT LL Header

--- llsmartcard/parser.py
+++ llsmartcard/parser.py
@@ -1,3 +1,7 @@
+# Copyright 2015, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: BSD-3-Clause
+
 # Native
 import logging
 import sys
