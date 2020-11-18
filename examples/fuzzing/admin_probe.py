a4e6c74e68ed2ccc75b482b8b0f126dca306dc8f
Lou Bellaire <r.louis.bellaire@ll.mit.edu>
2020-11-18 15:16:06 -0500

    Update admin_probe.py
    MIT LL header added

--- examples/fuzzing/admin_probe.py
+++ examples/fuzzing/admin_probe.py
@@ -1,4 +1,8 @@
 """
+ Copyright 2015, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+ Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+ SPDX-License-Identifier: BSD-3-Clause
+
     This is just a simple program to test all instructions in a given class
 """
 # Navtive
